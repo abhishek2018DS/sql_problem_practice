1975efd968314df623ccca5ed0d5fe2dcc488a97
abhishek2018DS <42770193+abhishek2018DS@users.noreply.github.com>
2023-11-30 17:11:51 +0530

    Update employee_database_queries_part_2.sql

--- employee_database_queries_part_2.sql
+++ employee_database_queries_part_2.sql
@@ -1,1 +1,270 @@
+/*problem 11. From the following table, write a SQL query to find those employees who do not belong to the department 2001. 
+Return complete information about the employees.
+Sample table: employees
+emp_id | emp_name | job_name  | manager_id | hire_date  | salary  | commission | dep_id
+--------+----------+-----------+------------+------------+---------+------------+--------
+  68319 | KAYLING  | PRESIDENT |            | 1991-11-18 | 6000.00 |            |   1001
+  66928 | BLAZE    | MANAGER   |      68319 | 1991-05-01 | 2750.00 |            |   3001
+  67832 | CLARE    | MANAGER   |      68319 | 1991-06-09 | 2550.00 |            |   1001
+  65646 | JONAS    | MANAGER   |      68319 | 1991-04-02 | 2957.00 |            |   2001
+  67858 | SCARLET  | ANALYST   |      65646 | 1997-04-19 | 3100.00 |            |   2001
+  69062 | FRANK    | ANALYST   |      65646 | 1991-12-03 | 3100.00 |            |   2001
+  63679 | SANDRINE | CLERK     |      69062 | 1990-12-18 |  900.00 |            |   2001
+  64989 | ADELYN   | SALESMAN  |      66928 | 1991-02-20 | 1700.00 |     400.00 |   3001
+  65271 | WADE     | SALESMAN  |      66928 | 1991-02-22 | 1350.00 |     600.00 |   3001
+  66564 | MADDEN   | SALESMAN  |      66928 | 1991-09-28 | 1350.00 |    1500.00 |   3001
+  68454 | TUCKER   | SALESMAN  |      66928 | 1991-09-08 | 1600.00 |       0.00 |   3001
+  68736 | ADNRES   | CLERK     |      67858 | 1997-05-23 | 1200.00 |            |   2001
+  69000 | JULIUS   | CLERK     |      66928 | 1991-12-03 | 1050.00 |            |   3001
+  69324 | MARKER   | CLERK     |      67832 | 1992-01-23 | 1400.00 |            |   1001
+(14 rows)
+
+output:
+emp_id | emp_name | job_name  | manager_id | hire_date  | salary  | commission | dep_id
+--------+----------+-----------+------------+------------+---------+------------+--------
+  68319 | KAYLING  | PRESIDENT |            | 1991-11-18 | 6000.00 |            |   1001
+  66928 | BLAZE    | MANAGER   |      68319 | 1991-05-01 | 2750.00 |            |   3001
+  67832 | CLARE    | MANAGER   |      68319 | 1991-06-09 | 2550.00 |            |   1001
+  64989 | ADELYN   | SALESMAN  |      66928 | 1991-02-20 | 1700.00 |     400.00 |   3001
+  ....
+*/
+
+SELECT *
+FROM employees
+WHERE dep_id NOT IN (2001);
+
+
+/*problem 12:  From the following table, write a SQL query to find those employees who joined before 1991. 
+Return complete information about the employees.
+emp_id | emp_name | job_name  | manager_id | hire_date  | salary  | commission | dep_id
+--------+----------+-----------+------------+------------+---------+------------+--------
+  68319 | KAYLING  | PRESIDENT |            | 1991-11-18 | 6000.00 |            |   1001
+  66928 | BLAZE    | MANAGER   |      68319 | 1991-05-01 | 2750.00 |            |   3001
+  67832 | CLARE    | MANAGER   |      68319 | 1991-06-09 | 2550.00 |            |   1001
+  65646 | JONAS    | MANAGER   |      68319 | 1991-04-02 | 2957.00 |            |   2001
+  67858 | SCARLET  | ANALYST   |      65646 | 1997-04-19 | 3100.00 |            |   2001
+  69062 | FRANK    | ANALYST   |      65646 | 1991-12-03 | 3100.00 |            |   2001
+  63679 | SANDRINE | CLERK     |      69062 | 1990-12-18 |  900.00 |            |   2001
+  64989 | ADELYN   | SALESMAN  |      66928 | 1991-02-20 | 1700.00 |     400.00 |   3001
+  65271 | WADE     | SALESMAN  |      66928 | 1991-02-22 | 1350.00 |     600.00 |   3001
+  66564 | MADDEN   | SALESMAN  |      66928 | 1991-09-28 | 1350.00 |    1500.00 |   3001
+  68454 | TUCKER   | SALESMAN  |      66928 | 1991-09-08 | 1600.00 |       0.00 |   3001
+  68736 | ADNRES   | CLERK     |      67858 | 1997-05-23 | 1200.00 |            |   2001
+  69000 | JULIUS   | CLERK     |      66928 | 1991-12-03 | 1050.00 |            |   3001
+  69324 | MARKER   | CLERK     |      67832 | 1992-01-23 | 1400.00 |            |   1001
+(14 rows)
+output:
+emp_id | emp_name | job_name | manager_id | hire_date  | salary | commission | dep_id
+--------+----------+----------+------------+------------+--------+------------+--------
+  63679 | SANDRINE | CLERK    |      69062 | 1990-12-18 | 900.00 |            |   2001
+(1 row)
+*/
+SELECT *
+FROM employees
+WHERE hire_date<('1991-1-1');
+
+
+
+/*Problem 13: From the following table, write a SQL query to calculate the average salary of employees who work as analysts. Return average salary.
+Sample table: employees
+emp_id | emp_name | job_name  | manager_id | hire_date  | salary  | commission | dep_id
+--------+----------+-----------+------------+------------+---------+------------+--------
+  68319 | KAYLING  | PRESIDENT |            | 1991-11-18 | 6000.00 |            |   1001
+  66928 | BLAZE    | MANAGER   |      68319 | 1991-05-01 | 2750.00 |            |   3001
+  67832 | CLARE    | MANAGER   |      68319 | 1991-06-09 | 2550.00 |            |   1001
+  65646 | JONAS    | MANAGER   |      68319 | 1991-04-02 | 2957.00 |            |   2001
+  67858 | SCARLET  | ANALYST   |      65646 | 1997-04-19 | 3100.00 |            |   2001
+  69062 | FRANK    | ANALYST   |      65646 | 1991-12-03 | 3100.00 |            |   2001
+  63679 | SANDRINE | CLERK     |      69062 | 1990-12-18 |  900.00 |            |   2001
+  64989 | ADELYN   | SALESMAN  |      66928 | 1991-02-20 | 1700.00 |     400.00 |   3001
+  65271 | WADE     | SALESMAN  |      66928 | 1991-02-22 | 1350.00 |     600.00 |   3001
+  66564 | MADDEN   | SALESMAN  |      66928 | 1991-09-28 | 1350.00 |    1500.00 |   3001
+  68454 | TUCKER   | SALESMAN  |      66928 | 1991-09-08 | 1600.00 |       0.00 |   3001
+  68736 | ADNRES   | CLERK     |      67858 | 1997-05-23 | 1200.00 |            |   2001
+  69000 | JULIUS   | CLERK     |      66928 | 1991-12-03 | 1050.00 |            |   3001
+  69324 | MARKER   | CLERK     |      67832 | 1992-01-23 | 1400.00 |            |   1001
+(14 rows)
+
+output:
+ avg
+-----------------------
+ 3100.0000000000000000
+(1 row)
+*/
+SELECT avg(salary)
+FROM employees
+WHERE job_name = 'ANALYST';
+
+
+/*problem 14: From the following table, write a SQL query to find the details of the employee ‘BLAZE’.
+
+Sample table: employees
+
+emp_id | emp_name | job_name  | manager_id | hire_date  | salary  | commission | dep_id
+--------+----------+-----------+------------+------------+---------+------------+--------
+  68319 | KAYLING  | PRESIDENT |            | 1991-11-18 | 6000.00 |            |   1001
+  66928 | BLAZE    | MANAGER   |      68319 | 1991-05-01 | 2750.00 |            |   3001
+  67832 | CLARE    | MANAGER   |      68319 | 1991-06-09 | 2550.00 |            |   1001
+  65646 | JONAS    | MANAGER   |      68319 | 1991-04-02 | 2957.00 |            |   2001
+  67858 | SCARLET  | ANALYST   |      65646 | 1997-04-19 | 3100.00 |            |   2001
+  69062 | FRANK    | ANALYST   |      65646 | 1991-12-03 | 3100.00 |            |   2001
+  63679 | SANDRINE | CLERK     |      69062 | 1990-12-18 |  900.00 |            |   2001
+  64989 | ADELYN   | SALESMAN  |      66928 | 1991-02-20 | 1700.00 |     400.00 |   3001
+  65271 | WADE     | SALESMAN  |      66928 | 1991-02-22 | 1350.00 |     600.00 |   3001
+  66564 | MADDEN   | SALESMAN  |      66928 | 1991-09-28 | 1350.00 |    1500.00 |   3001
+  68454 | TUCKER   | SALESMAN  |      66928 | 1991-09-08 | 1600.00 |       0.00 |   3001
+  68736 | ADNRES   | CLERK     |      67858 | 1997-05-23 | 1200.00 |            |   2001
+  69000 | JULIUS   | CLERK     |      66928 | 1991-12-03 | 1050.00 |            |   3001
+  69324 | MARKER   | CLERK     |      67832 | 1992-01-23 | 1400.00 |            |   1001
+(14 rows)
+output:
+emp_id | emp_name | job_name | manager_id | hire_date  | salary  | commission | dep_id
+--------+----------+----------+------------+------------+---------+------------+--------
+  66928 | BLAZE    | MANAGER  |      68319 | 1991-05-01 | 2750.00 |            |   3001
+(1 row)
+*/
+SELECT *
+FROM employees
+WHERE emp_name = 'BLAZE';
+
+
+
+/*Problem 15: From the following table, write a SQL query to identify employees whose commissions exceed their salaries.
+Return complete information about the employees.
+Sample table: employees
+emp_id | emp_name | job_name  | manager_id | hire_date  | salary  | commission | dep_id
+--------+----------+-----------+------------+------------+---------+------------+--------
+  68319 | KAYLING  | PRESIDENT |            | 1991-11-18 | 6000.00 |            |   1001
+  66928 | BLAZE    | MANAGER   |      68319 | 1991-05-01 | 2750.00 |            |   3001
+  67832 | CLARE    | MANAGER   |      68319 | 1991-06-09 | 2550.00 |            |   1001
+  65646 | JONAS    | MANAGER   |      68319 | 1991-04-02 | 2957.00 |            |   2001
+  67858 | SCARLET  | ANALYST   |      65646 | 1997-04-19 | 3100.00 |            |   2001
+  69062 | FRANK    | ANALYST   |      65646 | 1991-12-03 | 3100.00 |            |   2001
+  63679 | SANDRINE | CLERK     |      69062 | 1990-12-18 |  900.00 |            |   2001
+  64989 | ADELYN   | SALESMAN  |      66928 | 1991-02-20 | 1700.00 |     400.00 |   3001
+  65271 | WADE     | SALESMAN  |      66928 | 1991-02-22 | 1350.00 |     600.00 |   3001
+  66564 | MADDEN   | SALESMAN  |      66928 | 1991-09-28 | 1350.00 |    1500.00 |   3001
+  68454 | TUCKER   | SALESMAN  |      66928 | 1991-09-08 | 1600.00 |       0.00 |   3001
+  68736 | ADNRES   | CLERK     |      67858 | 1997-05-23 | 1200.00 |            |   2001
+  69000 | JULIUS   | CLERK     |      66928 | 1991-12-03 | 1050.00 |            |   3001
+  69324 | MARKER   | CLERK     |      67832 | 1992-01-23 | 1400.00 |            |   1001
+(14 rows)
+
+output:
+emp_id | emp_name | job_name | manager_id | hire_date  | salary  | commission | dep_id
+--------+----------+----------+------------+------------+---------+------------+--------
+  66564 | MADDEN   | SALESMAN |      66928 | 1991-09-28 | 1350.00 |    1500.00 |   3001
+(1 row)
+*/
+
+SELECT *
+FROM employees
+WHERE commission>salary;
+
+
+/*problem 16:  From the following table, write a SQL query to identify those employees whose salaries exceed 3000 after receiving a 25% salary increase.
+Return complete information about the employees.
+Sample table: employees
+emp_id | emp_name | job_name  | manager_id | hire_date  | salary  | commission | dep_id
+--------+----------+-----------+------------+------------+---------+------------+--------
+  68319 | KAYLING  | PRESIDENT |            | 1991-11-18 | 6000.00 |            |   1001
+  66928 | BLAZE    | MANAGER   |      68319 | 1991-05-01 | 2750.00 |            |   3001
+  67832 | CLARE    | MANAGER   |      68319 | 1991-06-09 | 2550.00 |            |   1001
+  65646 | JONAS    | MANAGER   |      68319 | 1991-04-02 | 2957.00 |            |   2001
+  67858 | SCARLET  | ANALYST   |      65646 | 1997-04-19 | 3100.00 |            |   2001
+  69062 | FRANK    | ANALYST   |      65646 | 1991-12-03 | 3100.00 |            |   2001
+  63679 | SANDRINE | CLERK     |      69062 | 1990-12-18 |  900.00 |            |   2001
+  64989 | ADELYN   | SALESMAN  |      66928 | 1991-02-20 | 1700.00 |     400.00 |   3001
+  65271 | WADE     | SALESMAN  |      66928 | 1991-02-22 | 1350.00 |     600.00 |   3001
+  66564 | MADDEN   | SALESMAN  |      66928 | 1991-09-28 | 1350.00 |    1500.00 |   3001
+  68454 | TUCKER   | SALESMAN  |      66928 | 1991-09-08 | 1600.00 |       0.00 |   3001
+  68736 | ADNRES   | CLERK     |      67858 | 1997-05-23 | 1200.00 |            |   2001
+  69000 | JULIUS   | CLERK     |      66928 | 1991-12-03 | 1050.00 |            |   3001
+  69324 | MARKER   | CLERK     |      67832 | 1992-01-23 | 1400.00 |            |   1001
+(14 rows)
+output:
+emp_id | emp_name | job_name  | manager_id | hire_date  | salary  | commission | dep_id
+--------+----------+-----------+------------+------------+---------+------------+--------
+  68319 | KAYLING  | PRESIDENT |            | 1991-11-18 | 6000.00 |            |   1001
+  66928 | BLAZE    | MANAGER   |      68319 | 1991-05-01 | 2750.00 |            |   3001
+  67832 | CLARE    | MANAGER   |      68319 | 1991-06-09 | 2550.00 |            |   1001
+  65646 | JONAS    | MANAGER   |      68319 | 1991-04-02 | 2957.00 |            |   2001
+  ....
+*/
+
+SELECT *
+FROM employees
+WHERE commission>salary;
+
+
+
+/*Problem 17:  From the above table, 
+write a SQL query to find the names of the employees whose length is six. Return employee name.
+
+output:
+emp_name
+----------
+ ADELYN
+ MADDEN
+ TUCKER
+ ADNRES
+ JULIUS
+ MARKER
+(6 rows)
+*/
+SELECT emp_name
+FROM employees
+WHERE length(emp_name)=6;
+
+
+
+
+/*Problem 18: From the above table,
+ write a SQL query to find out which employees joined in the month of January. Return complete information about the employees.
+output:
+emp_id | emp_name | job_name | manager_id | hire_date  | salary  | commission | dep_id
+--------+----------+----------+------------+------------+---------+------------+--------
+  69324 | MARKER   | CLERK    |      67832 | 1992-01-23 | 1400.00 |            |   1001
+(1 row)
+*/
+
+SELECT *
+FROM employees
+WHERE to_char(hire_date, 'mon')='jan';
+
+
+
+
+/*Problem 19: From the  above table, write a SQL query to separate the names of employees and their managers by the string 'works for'.
+output:
+?column?
+--------------------------
+ BLAZE works for KAYLING
+ CLARE works for KAYLING
+ JONAS works for KAYLING
+ SCARLET works for JONAS
+ FRANK works for JONAS
+ .....
+*/
+
+SELECT e.emp_name || ' works for ' || m.emp_name
+FROM employees e,
+     employees m
+WHERE e.manager_id = m.emp_id;
+
+
+
+/*Problem 20: From the above table, write a SQL query to find those employees whose designation is ‘CLERK’. Return complete information about the employees.
+output: 
+emp_id | emp_name | job_name | manager_id | hire_date  | salary  | commission | dep_id
+--------+----------+----------+------------+------------+---------+------------+--------
+  63679 | SANDRINE | CLERK    |      69062 | 1990-12-18 |  900.00 |            |   2001
+  68736 | ADNRES   | CLERK    |      67858 | 1997-05-23 | 1200.00 |            |   2001
+  69000 | JULIUS   | CLERK    |      66928 | 1991-12-03 | 1050.00 |            |   3001
+  69324 | MARKER   | CLERK    |      67832 | 1992-01-23 | 1400.00 |            |   1001
+(4 rows)
+*/
+SELECT *
+FROM employees
+WHERE job_name = 'CLERK';
 
